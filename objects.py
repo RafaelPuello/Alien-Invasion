df852d8a6b037a65ada2c925a9cd54d91aed283d
Rafael Puello <puello322@gmail.com>
2019-03-03 17:51:43 -0500

    Started working on game objects

--- objects.py
+++ objects.py
@@ -1,1 +1,16 @@
 """ Module used to hold the game objects. """
+
+try:
+    import pygame
+
+except ImportError as error:
+    sys.exit("Couldn't load module.  {}".format(error))
+
+
+class UserShip(pygame.sprite.Sprite):
+    """ Class used for player spaceship. """
+
+    def __init__(self):
+        """ Initialize user spaceship. """
+
+        pygame.sprite.Sprite.__init__(self)
